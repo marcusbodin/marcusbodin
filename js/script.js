18077593e4501809d2ec52e31687e1298af98860
Marcus Bodin <Marcus Bodin>
2016-12-18 13:55:19 +0100

    hej

--- js/script.js
+++ js/script.js
@@ -4,8 +4,17 @@ window.addEventListener('scroll', function () {
   ]('scrolled');
 });
 
-$(document).ready(function(){
+$(document).ready(function(e){
 	var btt = $('.backtotop');
+
+	btt.on('click', function(){
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+
+		e.preventDefault();
+	});
+
 	$(window).on('scroll', function(){
 		console.log('scrolled');
 		var self = $(this),
@@ -22,3 +31,8 @@ $(document).ready(function(){
 	});
 
 });
+
+function scroll_to_anchor(anchor_id){
+    var tag = $("#"+anchor_id+"");
+    $('html,body').animate({scrollTop: tag.offset().top},'slow');
+}
