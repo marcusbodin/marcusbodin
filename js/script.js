94f49f051f68b3c06c924c5d705f45978d31f04f
Marcus Bodin <Marcus Bodin>
2017-07-13 12:45:15 +0200

    formatering för mobilläge

--- js/script.js
+++ js/script.js
@@ -1,38 +1,5 @@
-window.addEventListener('scroll', function () {
-  document.body.classList[
-    window.scrollY > 0 ? 'add': 'remove'
-  ]('scrolled');
-});
-
-$(document).ready(function(e){
-	var btt = $('.backtotop');
-
-	btt.on('click', function(){
-		$('html, body').animate({
-			scrollTop: 710
-		}, 500);
-
-		e.preventDefault();
-	});
-
-	$(window).on('scroll', function(){
-		console.log('scrolled');
-		var self = $(this),
-			height = self.height(),
-			top = self.scrollTop();
-
-		if(top > height){
-			if(!btt.is(':visible')){
-				btt.show();
-			}
-		}	else {
-			btt.hide();
-		}
-	});
-
-});
-
-function scroll_to_anchor(anchor_id){
-    var tag = $("#"+anchor_id+"");
-    $('html,body').animate({scrollTop: tag.offset().top},'slow');
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
 }
