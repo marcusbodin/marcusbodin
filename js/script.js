9687f22d051e3c1501af8fed3df68586ea984d3d
Marcus Bodin <Marcus Bodin>
2017-07-18 01:09:11 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -6,33 +6,38 @@ function myFunction() {
 
 var hide = "off";
 
-if(window.innerHeight > window.innerWidth){
+
 	$(window).scroll(function (event) {
-	    var scroll = $(window).scrollTop();
-	    if (scroll > 50 && hide == "off"){
-			var myImg = document.getElementById('portrait');
-			if(myImg && myImg.style) {
-			    myImg.style.height = '0vh';
+		var scroll = $(window).scrollTop();
+		console.log(scroll)
+		if(window.innerHeight > window.innerWidth){
+		    if (scroll > 50 && hide == "off"){
+				var myImg = document.getElementById('portrait');
+				if(myImg && myImg.style) {
+					var imgHeight = 100 - scroll * 1.5 +"vh";
+					console.log("THE IMAGE HEIGHT IS" + imgHeight)
+				    myImg.style.height = imgHeight;
+				    
+				}
+				hide = "on";
+				var mySec = document.getElementById('icon-box');
+				if(mySec && mySec.style) {
+				    mySec.style.visibility = 'hidden';
+				}
 			}
-			hide = "on";
-			var mySec = document.getElementById('icon-box');
-			if(mySec && mySec.style) {
-			    mySec.style.visibility = 'hidden';
-			}
-		}
-		else if (hide == "on" && scroll == 0){
-			var myImg = document.getElementById('portrait');
-			if(myImg && myImg.style) {
-				myImg.style.height = '50vh';
-			}
-			hide = "off";
-			var mySec = document.getElementById('icon-box');
-			if(mySec && mySec.style) {
-			    mySec.style.visibility = 'visible';
+			else if (hide == "on" && scroll == 0){
+				var myImg = document.getElementById('portrait');
+				if(myImg && myImg.style) {
+					myImg.style.height = '50vh';
+				}
+				hide = "off";
+				var mySec = document.getElementById('icon-box');
+				if(mySec && mySec.style) {
+				    mySec.style.visibility = 'visible';
+				}
 			}
 		}
 	});
-}
 
 window.addEventListener('resize', function(){
     if(window.innerHeight < window.innerWidth){
