1117dc74e61843a296aff01deeaf1d23938ef1fa
Marcus Bodin <Marcus Bodin>
2017-07-28 14:03:04 +0200

    nytt

--- js/script.js
+++ js/script.js
@@ -1,21 +1,12 @@
-// When the user clicks on <div>, open the popup
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-}
-
+// Funktion för att minska profilbild på mobilvy
 var hide = "off";
-
-
 	$(window).scroll(function (event) {
 		var scroll = $(window).scrollTop();
-		console.log(scroll)
 		if(window.innerHeight > window.innerWidth){
 		    if (scroll > 50 && hide == "off"){
 				var myImg = document.getElementById('portrait');
 				if(myImg && myImg.style) {
 					var imgHeight = 20 +"vh";
-					console.log("THE IMAGE HEIGHT IS" + imgHeight)
 				    myImg.style.height = imgHeight;
 				    
 				}
@@ -53,3 +44,31 @@ window.addEventListener('resize', function(){
     }
 });
 
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
